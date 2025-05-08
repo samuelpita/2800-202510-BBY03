1a898402072ca7b691a8168bcbc09fe61cada32f
diananzrn@gmail.com <diananzrn@gmail.com>
2025-05-08 12:17:35 -0700

    Made links to pages appear as orange buttons

--- src/routes/LandingNavBar.svelte
+++ src/routes/LandingNavBar.svelte
@@ -1,11 +1,11 @@
-<div class="h-nav bg-light-2 dark:bg-dark-2 fixed top-0 flex w-full items-center justify-between">
+<div class="h-nav bg-light-2 dark:bg-dark-2 fixed top-0 flex w-full items-center justify-between"> 
     <a href="/" class="flex">
-        <img src="https://dummyimage.com/128x128/000/fff" alt="" class="size-nav" />
+        <img src="/logo.png" alt="" class="size-nav" />
     </a>
     <nav class="flex gap-4">
-        <a href="/app" class="text-link">/app</a>
-        <a href="/login" class="text-link">/login</a>
-        <a href="/register" class="text-link">/register</a>
+        <a href="/app" class="btn-orange">App</a>
+        <a href="/login" class="btn-orange">Login</a>
+        <a href="/register" class="btn-orange">Register</a>
     </nav>
 </div>
 
